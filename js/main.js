6836582351e723c1c0a3546cfcf02e7e51c511b9
callixtus12 <callistusedoziuno@gmail.com>
2024-10-15 16:40:10 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+const menu = document.querySelector(".ri-menu-line")
+const close = document.querySelector(".ri-close-line")
+const nav = document.querySelector("nav ul")
+
+menu.addEventListener('click', ()=>{
+    nav.style.top = '30%'
+    menu.style.display = 'none'
+    close.style.display = 'block'
+})
+
+close.addEventListener('click', ()=>{
+    nav.style.top = '-30%'
+    menu.style.display = 'block'
+    close.style.display = 'none'
+})
